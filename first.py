c1f636d6b635247091937a4ea8eda9b709cc2f27
164030-cell <164030@wsk.ac.th>
2026-06-11 02:56:42 +0000

    first.py

---
+++ first.py
@@ -0,0 +1,11 @@
+# โปรแกรมคำนวณพื้นที่สี่เหลี่ยมผืนผ้า
+
+width = float(input("กรอกความกว้าง: "))
+length = float(input("กรอกความยาว: "))
+
+area = width * length
+
+print("\nผลการคำนวณ")
+print(f"ความกว้าง = {width}")
+print(f"ความยาว = {length}")
+print(f"พื้นที่ = {area}")
